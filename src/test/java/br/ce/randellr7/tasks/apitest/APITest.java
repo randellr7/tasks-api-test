a5e1fd422b6f892c0a2e98c2ec02c4487dad4c4f
unknown <Admin@DESKTOP-LD4HF96.hapvida.com.br>
2021-06-22 23:47:33 -0300

    teste de remocao

--- src/test/java/br/ce/randellr7/tasks/apitest/APITest.java
+++ src/test/java/br/ce/randellr7/tasks/apitest/APITest.java
@@ -50,6 +50,30 @@ public class APITest {
 		;
 	}
 	
+	@Test
+	public void deveRemoverTarefaComSucesso() {
+		//inserir
+		Integer id = RestAssured.given()
+			.body("{ \"task\": \"tarefa teste wwww\" , \"dueDate\": \"2021-12-30\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.extract().path("id")
+		;
+		
+		//remover
+		RestAssured.given()
+		.when()
+			.delete("/todo/"+id)
+		.then()
+			.statusCode(204)
+		;
+	}
+	
+	
 }
 
 
